1b154e1b8c3865d2100c70f71850c7e5cc8e3992
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-06-11 18:13:11 +0100

    done

--- api/v1/app.py
+++ api/v1/app.py
@@ -31,6 +31,6 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
