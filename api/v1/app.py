3d228331f3b5a80f30729f463888b13e225cab25
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-06-12 02:44:35 +0100

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Flask app initialization.
 """
 
-from flask import Flask
+from flask import Flask, jsonfy
 from models import storage
 from api.v1.views import app_views
 import os
@@ -19,8 +19,13 @@ def teardown_db(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors and returns a JSON-formatted response"""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
-
