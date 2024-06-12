85a9235dc2bb9e032bb1849fdbc152765b7e17d8
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-06-12 04:16:44 +0100

    done

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,6 +14,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes storage on teardown"""
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,13 +5,14 @@ Initializes the views module.
 
 from flask import Blueprint
 
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+if __name__ == "__init__":
+    from api.v1.views.index import *
+    from api.v1.views.states import *
+    from api.v1.views.cities import *
+    from api.v1.views.amenities import *
+    from api.v1.views.users import *
+    from api.v1.views.places import *
+    from api.v1.views.places_reviews import *
